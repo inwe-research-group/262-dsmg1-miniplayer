1be6e4362b7e14f8fb3ac80cab43598bef463c1d
inwe-research-group <zmamanir@unmsm.edu.pe>
2026-08-31 19:53:28 -0500

    add LoginScreen

--- app/src/main/java/com/dsm/miniplayer/navigation/NavigationWrapper.kt
+++ app/src/main/java/com/dsm/miniplayer/navigation/NavigationWrapper.kt
@@ -5,6 +5,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.dsm.miniplayer.ui.screens.initial.InitialScreen
+import com.dsm.miniplayer.ui.screens.login.LoginScreen
 import com.google.firebase.auth.FirebaseAuth
 
 @Composable
@@ -21,10 +22,10 @@ fun NavigationWrapper(
         }
 
         composable("logIn") {
-            /*LoginScreen(
+            LoginScreen(
                 auth,
-                navigateToSignUp={navHostController.navigate("signUp")}
-            )*/
+                onSignUp={navHostController.navigate("signUp")}
+            )
 
         }
 
