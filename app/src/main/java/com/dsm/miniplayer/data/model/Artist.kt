9904cb3c45fae965b71f7bfd94ad1ab9a43b1ecf
inwe-research-group <zmamanir@unmsm.edu.pe>
2026-09-14 20:08:25 -0500

    add Player and Song data class and modify Artist

--- app/src/main/java/com/dsm/miniplayer/data/model/Artist.kt
+++ app/src/main/java/com/dsm/miniplayer/data/model/Artist.kt
@@ -1,6 +1,7 @@
 package com.dsm.miniplayer.data.model
 
 data class Artist(
+    val artistId: Int = 0,
     val name: String?=null,
     val description: String?=null,
     val image: String?=null,
